5e615bae2767e31276e90a810af9c6fa922820bc
iswoit <iswoit_2_you@hotmail.com>
2017-10-30 10:06:41 +0800

    结构调整

---
+++ ClearfileCheckManager/Model/ClearFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearfileCheckModel
+{
+    public class ClearFile
+    {
+        private string _fileName;
+        private bool _isCopied;
+        private bool _isMD5Equal;
+        private string _fileSourcePath;
+        private string _fileDestPath;
+
+    }
+}
